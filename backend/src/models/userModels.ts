b68159a928fded3d1bb43905c9e9ac21e7d68077
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-07-10 23:15:40 +0800

    remove mysql

--- backend/src/models/userModels.ts
+++ backend/src/models/userModels.ts
@@ -1,28 +1,3 @@
-// export interface IFields {
-//   name: string;
-//   age: number;
-// }
-
-// const tableName: string = "users";
-
-// class User {
-//   find() {
-//     return `SELECT * FROM ${tableName}`;
-//   }
-
-//   create({ name, age }: IFields) {
-//     return `INSERT INTO ${tableName} (name, age) VALUES ('${name}','${age}')`;
-//   }
-
-//   findById(whereClause: {}) {
-//     return `SELECT * FROM ${tableName} WHERE ${Object.keys(whereClause)[0]} = ${
-//       Object.values(whereClause)[0]
-//     }`;
-//   }
-// }
-
-// export default User;
-
 import { Schema, models, model } from "mongoose";
 
 const UserSchema = new Schema(
