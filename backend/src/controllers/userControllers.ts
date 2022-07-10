728f562e96bc886cd2eeb32fc641ab93643b6f3f
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-07-10 20:11:09 +0800

    updateUser

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -29,3 +29,14 @@ export const getSingleUser = async (req: Request, res: Response) => {
     return res.status(401).json({ error });
   }
 };
+
+export const updateSingleUser = async (req: Request, res: Response) => {
+  try {
+    const _id = req.params.id;
+    const { name, age } = req.body;
+    const user = await User.findByIdAndUpdate({ _id }, { name, age });
+    return res.status(201).json({ message: "Success", user });
+  } catch (error) {
+    return res.status(401).json({ error });
+  }
+};
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -3,12 +3,13 @@ import {
   addUser,
   getAllUsers,
   getSingleUser,
+  updateSingleUser,
 } from "../controllers/userControllers";
 
 const router = express.Router();
 
 // router.route("/").get(getAllUsers);
 router.route("/").get(getAllUsers).post(addUser);
-router.route("/:id").get(getSingleUser);
+router.route("/:id").get(getSingleUser).put(updateSingleUser);
 
 export default router;
